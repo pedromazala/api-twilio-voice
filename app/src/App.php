6b1dcb96024d8e896185fea3909e6cd693ddf447
Pedro Mázala <pedroh.mazala@gmail.com>
2016-09-02 10:18:06 -0300

    Entidade responsável por iniciar o aplicativo

---
+++ app/src/App.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace EMiolo\Twilio;
+
+
+class App
+{
+    /**
+     * Inicia a aplicação reguperando a url base da aplicação.
+     * A constante __BASE_URL__ é criada
+     */
+    public static function start()
+    {
+        $protocol = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] : 'http';
+        $base_url = $_SERVER['HTTP_HOST'];
+        define('__BASE_URL__', $protocol . '://' . $base_url . '/');
+    }
+
+    /**
+     * Retorna os parâmetros passados na url
+     *
+     * @return array
+     */
+    public static function getParameters()
+    {
+        $params = explode("/", $_SERVER['REQUEST_URI']);
+
+        foreach ($params as $k => $p) {
+            if (strlen($p) == 0) {
+                unset($params[$k]);
+            } else {
+                continue;
+            }
+        }
+
+        return $params;
+    }
+
+    /**
+     * Verifica se a constante DEBUG está criada e setada como true
+     *
+     * @return bool
+     */
+    public static function isDebug()
+    {
+        if (defined('DEBUG') && DEBUG) {
+            return true;
+        }
+
+        return false;
+    }
+}
